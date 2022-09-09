bb1ef95293cb02447034378463fcb0f77393e32a
alex choi <heyalexchoi@gmail.com>
2022-09-09 11:56:12 -0400

    added fmdb files to xcode project and bridging header

--- native/ios/WatermelonDB/SupportingFiles/Bridging.h
+++ native/ios/WatermelonDB/SupportingFiles/Bridging.h
@@ -1,7 +1,9 @@
 #if __has_include("FMDB.h")
 #import "FMDB.h"
+#import "../extra/InMemoryOnDiskIO/FMDatabase+InMemoryOnDiskIO.h"
 #else
 #import "../FMDB/src/fmdb/FMDB.h"
+#import "../FMDB/src/extra/InMemoryOnDiskIO/FMDatabase+InMemoryOnDiskIO.h"
 #endif
 
 #import <React/RCTBridgeModule.h>
